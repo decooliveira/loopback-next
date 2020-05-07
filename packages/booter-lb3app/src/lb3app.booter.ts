600b46cf3657fd662b1890431abe61d37a58505f
jannyHou <juehou@ca.ibm.com>
2020-05-07 14:47:49 -0400

    feat(booter-lb3app): bind lb3 models
    Co-authored-by: nabdelgadir <nora.abdelgadir@ibm.com>"

--- packages/booter-lb3app/src/lb3app.booter.ts
+++ packages/booter-lb3app/src/lb3app.booter.ts
@@ -73,6 +73,17 @@ export class Lb3AppBooter implements Booter {
       });
     }
 
+    const models = lb3App.models;
+    if (models) {
+      const visited: unknown[] = [];
+      Object.keys(models).forEach(key => {
+        const model = models[key];
+        if (visited.includes(model)) return;
+        visited.push(model);
+        this.app.bind(`lb3-models.${key}`).to(model).tag('lb3-model');
+      });
+    }
+
     // TODO(bajtos) Listen for the following events to update the OpenAPI spec:
     // - modelRemoted
     // - modelDeleted
@@ -149,4 +160,6 @@ export interface Lb3AppBooterOptions {
 interface Lb3Application extends ExpressApplication {
   handler(name: 'rest'): ExpressRequestHandler;
   dataSources?: {[name: string]: unknown};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  models?: {[name: string]: any};
 }
